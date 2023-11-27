7ba4b605cd240b3de86c9e01791a504b74037e23
RjManhas <artaxnetworking@gmail.com>
2023-11-27 01:24:40 -0700

    📝 docs: undid change

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const config = {
   tagline: 'Fastify Essentials: Streamlining Success, Elevating Development',
   favicon: 'img/favicon.ico',
   // Set the production url of your site here
-  url: 'https://devoplx.github.io',
+  url: 'https://devoplx.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/fastify-essentials/',
